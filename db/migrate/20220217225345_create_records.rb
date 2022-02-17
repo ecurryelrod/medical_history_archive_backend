c72da53bb7ea7e30f795e92338d88839bc5fddf8
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-02-17 15:54:37 -0700

    rails scaffold generator for record

---
+++ db/migrate/20220217225345_create_records.rb
@@ -0,0 +1,16 @@
+class CreateRecords < ActiveRecord::Migration[6.1]
+  def change
+    create_table :records do |t|
+      t.string :doc_name
+      t.string :practice_name
+      t.string :url
+      t.date :date
+      t.bigInt :phone
+      t.string :medications
+      t.text :med_notes
+      t.text :comments
+
+      t.timestamps
+    end
+  end
+end
